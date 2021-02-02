ff5a512833d7937d0a9638dc4e155acca9a46ad1
lsw6684 <lsw6684@hanmail.net>
2021-02-02 15:37:29 +0900

    fuck..

--- indeed.py
+++ indeed.py
@@ -1,27 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages() :
+
+def extract_indeed_pages():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
-    pagination = soup.find("div", {"class":"pagination"})
+    pagination = soup.find("div", {"class": "pagination"})
     links = pagination.find_all('a')
     pages = []
 
-    for link in links[:-1]:             #[:-1] 원래 길이의 -1까지만
+    for link in links[:-1]:  # [:-1] 원래 길이의 -1까지만
         pages.append(int(link.string))  # .string = Only string
     max_page = pages[-1]
+    print(max_page)
     return max_page
 
+
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"}).find("a")
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {'title': title,
+            'company': company,
+            'location': location}
+
+  
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-    for result in results :
-        title = result.find("div", {"class": "title"})
-        print(title.find("a"})
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
